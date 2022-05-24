f299ddeb07ab114d02a69f1282839568fc0dc561
Su5eD <su5ed@outlook.com>
2022-05-24 11:49:20 +0200

    Publicize constructMethodDescriptor

--- src/main/kotlin/codes/som/koffee/util/MethodDescriptorConstruction.kt
+++ src/main/kotlin/codes/som/koffee/util/MethodDescriptorConstruction.kt
@@ -5,7 +5,7 @@ import codes.som.koffee.types.coerceType
 import org.objectweb.asm.Type
 
 @Suppress("NOTHING_TO_INLINE")
-internal inline fun constructMethodDescriptor(returnType: TypeLike, vararg parameterTypes: TypeLike): String {
+public inline fun constructMethodDescriptor(returnType: TypeLike, vararg parameterTypes: TypeLike): String {
     return Type.getMethodDescriptor(coerceType(returnType),
             *parameterTypes.map(::coerceType).toTypedArray())
 }
